29caa877e3d3a6a6cecd5dd486ce42db96d96082
radicale <radicale@users.noreply.github.com>
2021-12-21 11:44:03 +0100

    Bump and diversify versions

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from os.path import join, dirname
 
 setup(
     name='hm_pyhelper',
-    version='0.12.1',
-    author="Nebra Ltd",
-    author_email="support@nebra.com",
+    version='0.12.1rad',
+    author="Radicale",
+    author_email="none",
     description="Helium Python Helper",
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/NebraLtd/hm-pyhelper",
+    url="https://github.com/radicale/hm-pyhelper",
     install_requires=[
         'requests>=2.26.0',
         'jsonrpcclient==3.3.6',
         'retry==0.9.2'
     ],
     project_urls={
-        "Bug Tracker": "https://github.com/NebraLtd/hm-pyhelper/issues",
+        "Bug Tracker": "https://github.com/radicale/hm-pyhelper/issues",
     },
     packages=find_packages(),
     include_package_data=True
